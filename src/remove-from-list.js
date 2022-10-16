7419a80dbbc545551818c0b5af67cc3ee2742f21
Ольга Малюк <olhamaliuk@MacBook-Air-Olga.local>
2022-10-16 17:21:26 +0300

    feat: add stack

--- src/remove-from-list.js
+++ src/remove-from-list.js
@@ -1,6 +1,6 @@
-const { NotImplementedError } = require('../extensions/index.js');
+const { NotImplementedError } = require("../extensions/index.js");
 
-// const { ListNode } = require('../extensions/list-node.js');
+// const { ListNode } = require("../extensions/list-node.js");
 
 /**
  * Given a singly linked list of integers l and an integer k,
@@ -22,11 +22,25 @@ const { NotImplementedError } = require('../extensions/index.js');
  *   }
  * }
  */
-function removeKFromList(/* l, k */) {
-  throw new NotImplementedError('Not implemented');
-  // remove line with error and write your code here
+
+class ListNode {
+  constructor(x) {
+    this.value = x;
+    this.next = null;
+  }
+}
+function removeKFromList(l, k) {
+  if (!l) {
+    return null;
+  }
+  if (l.value === k) {
+    return l.next ? removeKFromList(l.next, k) : null;
+  } else {
+    l.next = removeKFromList(l.next, k)
+  }
+  return l;
 }
 
 module.exports = {
-  removeKFromList
+  removeKFromList,
 };
